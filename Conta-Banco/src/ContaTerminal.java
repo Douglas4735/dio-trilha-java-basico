d33b40b63db3c941fd81b276ae3150eec11ad486
douglas4735 <douglas4735@gmail.com>
2024-02-17 14:45:35 -0400

    novas altarações

--- Conta-Banco/src/ContaTerminal.java
+++ Conta-Banco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       Scanner sc = new Scanner(System.in);
+
+       System.out.println("digite seu Nome:");
+       String nomeCliente = sc.next();
+
+       System.out.println("Por favor digite o numero da sua Agéncia  :");
+       int agencia = sc.nextInt();
+       
+       System.out.println("Agora digite o numero da sua Conta com Digito:");
+       String numero = sc.next();
+
+       System.out.println("digite valor do Saque?:");
+       double saldo = sc.nextDouble();
+
+       System.out.println("Olá " + nomeCliente + " obrigado por criar uma conta em nosso banco sua agéncia é " + agencia + " Conta " + numero + " e seu saldo R$: " + saldo + " já está diponivel para saque." );
+
+
+
     }
 }
